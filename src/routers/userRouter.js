bc222691eec1991c670daa4380a69737eb332e99
ssbinn <tnqls808300@gmail.com>
2022-07-19 16:24:42 +0900

    URL Parameters 코드 하단으로 정렬

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -4,8 +4,8 @@ import { see, logout, edit, remove } from "../controllers/userController";
 const userRouter = express.Router();
 
 userRouter.get("/logout", logout);
-userRouter.get("/:id", see);
 userRouter.get("/edit", edit);
 userRouter.get("/remove", remove);
+userRouter.get("/:id", see);
 
 export default userRouter;
