defbcbc0c60ea9ffd3d0442dbfaa41d5b21db935
ssbinn <tnqls808300@gmail.com>
2022-07-26 14:07:14 +0900

    영상 수정 페이지, exception - 후에 hashtag, id URI 예외처리 필요

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -22,10 +22,10 @@ export const postUpload = async (req, res) => {
         await Video.create({
             title: title,
             description: description,
-            hashtags: hashtags.split(",").map((tag) => `#${tag}`),
+            hashtags: hashtags.split(",").map((tag) => (tag.startsWith("#") ? tag : `#${tag}`)),
         });
 
-        return res.redirect("/");
+        return res.redirect("/");  // 동영상 업로드 완료 시 -> 메인 페이지로 이동
     }
     catch (error) {
         return res.render("upload", { pageTitle: "Upload", errMessage: error._message });
@@ -36,21 +36,40 @@ export const postUpload = async (req, res) => {
 export const watch = async (req, res) => {
     const id = req.params.id;
     const video = await Video.findById(id);
+
+    if (video === null) {
+        // URI의 id값을 (24byte가 되는 선에서) 하나 변경할 경우 video에 null이 담김
+        // 24byte를 넘어가면 뜨는 cannot found id 오류 해결 못함
+        return res.render("404", { pageTitle: "video not found" });
+    }
     return res.render("watch", { pageTitle: video.title, video: video });
 }
 
 
-export const getEdit = (req, res) => {
+export const getEdit = async (req, res) => {
     const { id } = req.params;
-    return res.render("edit", { pageTitle: `Editing: ${video.title}` });
+    const video = await Video.findById(id);
+
+    if (!video) {
+        return res.render("404", { pageTitle: "video not found" });
+    }
+    return res.render("edit", { pageTitle: `Edit ${video.title}`, video: video });
 }
 
 
-export const postEdit = (req, res) => {
-    const id = req.params.id;
-    const title = req.body.title;
+export const postEdit = async (req, res) => {
+    const { id } = req.params;
+    const { title, description, hashtags } = req.body;
+    const video = await Video.findById(id);
 
-    return res.redirect(`/videos/${id}`);  // 동영상 수정 페이지 save 누를 시 -> 동영상 조회 페이지로 이동
+    if (!video) {
+        return res.render("404", { pageTitle: "video not found" });
+    }
+    video.title = title;
+    video.description = description;
+    video.hashtags = hashtags.split(",").map((tag) => (tag.startsWith("#") ? tag : `#${tag}`));
+    await video.save();
+    return res.redirect(`/videos/${id}`);  // 동영상 수정 완료 시 -> 동영상 조회 페이지로 이동
 }
 
 
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -3,7 +3,6 @@ import { getUpload, postUpload, watch, getEdit, postEdit, deleteVideo } from "..
 
 const videoRouter = express.Router();
 
-// mongoDB id는 24 바이트 16진수로 정의되어 있기 때문에 정규식 수정함
 videoRouter.route("/upload").get(getUpload).post(postUpload);
 videoRouter.get("/:id([0-9a-f]{24})", watch);
 videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
