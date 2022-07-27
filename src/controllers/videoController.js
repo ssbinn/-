07943baa248a1c1eb52da8d2e81ceeedb83fab5d
ssbinn <tnqls808300@gmail.com>
2022-07-27 12:33:30 +0900

    영상 삭제, 검색(regular expression)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,12 +2,25 @@ import Video from "../models/Video"
 
 export const home = async (req, res) => {
     try {
-        const videos = await Video.find({});
+        const videos = await Video.find({}).sort({ createdAt: "desc" });
         return res.render("home", { pageTitle: "Home", videos });
     }
     catch (error) {
         return res.render("server-error", { error });
     }
+}
+
+
+export const watch = async (req, res) => {
+    const id = req.params.id;
+    const video = await Video.findById(id);
+
+    if (video === null) {
+        // URI의 id값을 (24byte가 되는 선에서) 하나 변경할 경우 video에 null이 담김
+        // 24byte를 넘어가면 뜨는 cannot found id 오류 해결 못함
+        return res.render("404", { pageTitle: "video not found" });
+    }
+    return res.render("watch", { pageTitle: video.title, video: video });
 }
 
 
@@ -30,19 +43,6 @@ export const postUpload = async (req, res) => {
     catch (error) {
         return res.render("upload", { pageTitle: "Upload", errMessage: error._message });
     }
-}
-
-
-export const watch = async (req, res) => {
-    const id = req.params.id;
-    const video = await Video.findById(id);
-
-    if (video === null) {
-        // URI의 id값을 (24byte가 되는 선에서) 하나 변경할 경우 video에 null이 담김
-        // 24byte를 넘어가면 뜨는 cannot found id 오류 해결 못함
-        return res.render("404", { pageTitle: "video not found" });
-    }
-    return res.render("watch", { pageTitle: video.title, video: video });
 }
 
 
@@ -76,6 +76,22 @@ export const postEdit = async (req, res) => {
 }
 
 
-export const deleteVideo = (req, res) => res.send("deleteVideo");
+export const deleteVideo = async (req, res) => {
+    const { id } = req.params;
+    await Video.findByIdAndDelete(id);
+    return res.redirect("/");
+}
 
-export const search = (req, res) => res.send("Search");
+export const search = async (req, res) => {
+    const { keyword } = req.query;
+    let videos = [];
+
+    if (keyword) {
+        videos = await Video.find({
+            title: {
+                $regex: new RegExp(keyword, "i") // contain 방식의 regular expression 생성, i: 대소문자 무시
+            }
+        });
+    }
+    return res.render("search", { pageTitle: "Search", videos });
+}
--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -2,7 +2,7 @@ import express from "express";
 import { home, search } from "../controllers/videoController";
 import { join, login } from "../controllers/userController";
 
-// search map과 user에서도 할 수 있도롱..
+// search를 map과 user에서도 할 수 있도롱..
 
 const globalRouter = express.Router();
 
