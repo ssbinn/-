c84e8e3c2531747b3ade1f5988f4bb49d4366234
ssbinn <tnqls808300@gmail.com>
2022-08-22 17:54:54 +0900

    유저 프로필 edit form 생성, route 보호를 위한 middleware 생성

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,12 +1,27 @@
 export const localsMiddleware = (req, res, next) => {
-    // console.log(req.session); 
-
     res.locals.siteName = "송글";
     res.locals.loggedIn = Boolean(req.session.loggedIn);  // 로그인 된 유저라면 true, 그렇지 않으면 false
-    res.locals.loggedInUser = req.session.user;
-
-
-    console.log(res.locals);
+    res.locals.loggedInUser = req.session.user || {}; // req.session.user가 undefined 일 수 있기 때문에 빈 object 코드 추가 - view에서 loggedInUser에 접근하려 하는데 로그인되어 있지 않으면 생기는 에러 해결
 
     next();
+}
+
+
+// 로그인하지 않은 사용자가 "/users/edit" 같이 보호하려는 페이지에 접속을 시도하는 경우 등을 막기위해 필요 
+export const loggedInOnlyMiddleware = (req, res, next) => {
+    if (req.session.loggedIn === true) {
+        next();  // user가 로그인되어 있다면 요청을 계속하게 함
+    } else {
+        return res.redirect("/");
+    }
+}
+
+
+// 로그인 한 사용자가 "/login" 같은 페이지에 접속을 시도하는 경우 등을 막기위해 필요 
+export const publicOnlyMiddleware = (req, res, next) => {
+    if (req.session.loggedIn !== true) {
+        next();
+    } else {
+        return res.redirect("/");
+    }
 }
--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,14 +1,15 @@
 import express from "express";
 import { home, search } from "../controllers/videoController";
 import { signUp, postSignUp, login, postLogin } from "../controllers/userController";
+import { publicOnlyMiddleware } from "../middlewares";
 
 // search를 map과 user에서도 할 수 있도롱..
 
 const rootRouter = express.Router();
 
 rootRouter.get("/", home);
-rootRouter.route("/signup").get(signUp).post(postSignUp);
-rootRouter.route("/login").get(login).post(postLogin);
+rootRouter.route("/signup").all(publicOnlyMiddleware).get(signUp).post(postSignUp);
+rootRouter.route("/login").all(publicOnlyMiddleware).get(login).post(postLogin);  // 로그인 한 유저는 접속할 수 없도록 publicOnly 미들웨어 추가 
 rootRouter.get("/search", search);
 
 export default rootRouter;
